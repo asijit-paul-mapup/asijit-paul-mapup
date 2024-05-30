d4cb1564eaf8a23e88ce75303d6d096cb5940032
Asijit Paul <121224792+asijit-paul-mapup@users.noreply.github.com>
2024-05-30 18:51:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const thisYear = new Date().getFullYear()
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressBarOfThisYear = generateProgressBar()
+
+function generateProgressBar() {
+    const progressBarCapacity = 30
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar = Array(progressBarCapacity)
+        .fill('▁')
+        .map((value, index) => index < passedProgressBarIndex ? '█' : value)
+        .join('')
+    return `{ ${progressBar} }`
+}
+
+const readme = `\
+### Hi there 👋
+
+[![Header](https://raw.githubusercontent.com/asijit-paul-mapup/asijit-paul-mapup/main/asijit_gif.gif)](https://www.linkedin.com/in/asijit-paul-2142881a2/)
+![Follow](https://img.shields.io/github/followers/asijit-paul-mapup?label=Follow&style=social)
+
+### Languages
+
+[![Python](https://img.shields.io/badge/-Python-000?&logo=python)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=python)
+[![PHP](https://img.shields.io/badge/-Php-000?&logo=Php)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=php)
+[![JavaScript](https://img.shields.io/badge/-JavaScript-000?&logo=JavaScript&logoColor=ddc508)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=javascript)
+[![C](https://img.shields.io/badge/-C-000?&logo=C)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=c)
+[![Java](https://img.shields.io/badge/-Java-000?&logo=Java&logoColor=007396)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=java)
+[![TypeScript](https://img.shields.io/badge/-TypeScript-000?&logo=TypeScript&logoColor=007ACC)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=typescript)
+[![C++](https://img.shields.io/badge/-C++-000?&logo=c%2b%2b&logoColor=00599C)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=cpp)
+[![Go](https://img.shields.io/badge/-Golang-000?&logo=go)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=go)
+[![SQL](https://img.shields.io/badge/-SQL-000?&logo=MySQL&logoColor=4479A1)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=sql)
+[![Swift](https://img.shields.io/badge/-Swift-000?&logo=Swift)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=swift)
+[![Node.js](https://img.shields.io/badge/-Node.js-000?&logo=node.js)](https://github.com/asijit-paul-mapup?tab=repositories&q=&type=&language=node)
+![React](https://img.shields.io/badge/-React-000?&logo=React)
+
+### Technologies
+
+![GIT](https://img.shields.io/badge/-Git-000?&logo=Git)
+![AWS](https://img.shields.io/badge/-AWS-000?&logo=Amazon-AWS&logoColor=FF9900)
+![CI/CD](https://img.shields.io/badge/-CI%2FCD-000?&logo=CircleCI&logoColor=888)
+![Docker](https://img.shields.io/badge/-Docker-000?&logo=Docker)
+![Jira](https://img.shields.io/badge/-Jira-000?&logo=Jira-Software&logoColor=0052CC)
+![Kubernetes](https://img.shields.io/badge/-Kubernetes-000?&logo=Kubernetes)
+![Linux](https://img.shields.io/badge/-Linux-000?&logo=Linux&logoColor=FCC624)
+![Jenkins](https://img.shields.io/badge/-Jenkins-000?&logo=Jenkins)
+
+## &#x1f4c8; GitHub Stats
+![Asijit's github stats](https://github-readme-stats.vercel.app/api?username=asijit-paul-mapup&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515)
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=asijit-paul-mapup&layout=compact&text_color=daf7dc&bg_color=151515)](https://github.com/anuraghazra/github-readme-stats)
+
+## YEAR PROGRESS BAR
+⏳ **Year Progress** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${new Date().getDate()+'-'+(new Date().getMonth()+1)+'-'+new Date().getFullYear()+'.'}
+
+## 🏆 GitHub Trophies
+
+[![trophy](https://github-profile-trophy.vercel.app/?username=asijit-paul-mapup&theme=nord&column=7)](https://github.com/ryo-ma/github-profile-trophy)
+
+
+## 📫 How to reach me
+𝚈𝚘𝚞 𝚌𝚊𝚗 𝚛𝚎𝚊𝚌𝚑 𝚖𝚎 𝚊𝚝 𝚝𝚑𝚎 𝚎𝚖𝚊𝚒𝚕 𝚒𝚗 𝚖𝚢 𝚐𝚒𝚝𝚑𝚞𝚋 𝚙𝚛𝚘𝚏𝚒𝚕𝚎. 𝙵𝚘𝚕𝚕𝚘𝚠 𝚖𝚢 𝚜𝚘𝚌𝚒𝚊𝚕𝚜!
+
+[<img src="https://raw.githubusercontent.com/Raymo111/Raymo111/master/socials/linkedin.png" height="40em" align="center" alt="Follow Raymo111 on LinkedIn" title="Follow Asijit on Instagram"/>](https://www.linkedin.com/in/asijit-paul-2142881a2/)
+[<img src="https://raw.githubusercontent.com/Raymo111/Raymo111/master/socials/twitter.svg" height="40em" align="center" alt="Follow Raym0111 on Twitter" title="Follow Asijit on Instagram"/>](https://twitter.com/asijit_paul)`
